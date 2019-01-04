88daad6e2876417d8f2d0f99a6b90bafe2bd4701
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-04 17:24:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
-display_board
+def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+end
